75ed9da162b28e0bfb6b9a8ed0dc1cb036d77bc7
KimWooSun <kipsong133@gmail.com>
2021-09-24 12:44:29 +0900

    initial commit

--- UndoManager_Example/ViewController.swift
+++ UndoManager_Example/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI Object
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var colorView: UIView!
+    
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    // MARK: - Actions
+    @IBAction func greenButtonDidtap(_ sender: UIButton) {
+        changeColorAndState(.green)
+    }
+    
+    @IBAction func purpleButtonDidtap(_ sender: UIButton) {
+        changeColorAndState(.purple)
+    }
+    
+    @IBAction func blueButtonDidTap(_ sender: UIButton) {
+        changeColorAndState(.blue)
+    }
+    
+    @IBAction func redButtonDidTap(_ sender: UIButton) {
+        changeColorAndState(.red)
+    }
+    
+    @IBAction func undoButtonDidTap(_ sender: UIButton) {
+        undoManager?.undo()
+        
+        stateLabel.text = "Undo is called"
+    }
+    
+    @IBAction func redoButtonDidTap(_ sender: UIButton) {
+        undoManager?.redo()
+        stateLabel.text = "Redo is called"
+    }
+    
+    // MARK: - Helpers
+    func changeColorAndState(_ color: UIColor) {
+        let old = self.colorView.backgroundColor ?? UIColor.white
+        
+        // 1. 이전 색상을 undoManager에 등록
+        undoManager?.registerUndo(withTarget: self, handler: { target in
+            target.changeColorAndState(old)
+        })
+        
+        // 2. 새롭게 입력받은 색상을 UIView에 입력
+        colorView.backgroundColor = color
+    }
 }
 
